bc45cd876315ee521184432294155a00a17f345e
heyoanne <souzayun@gmail.com>
2024-06-14 10:50:20 -0300

    Co-authored-by: KingCls <KingCls@users.noreply.github.com> Co-authored-by: Maria Eduarda R.Gomes <mariaeduardargomes@users.noreply.github.com> Co-authored-by: giimantovan <giimantovan@users.noreply.github.com> Co-authored-by: raissaredondaro <raissaredondaro@users.noreply.github.com> Co-authored-by: pxdrocosta <pxdrocosta@users.noreply.github.com>

--- windeeco/App.js
+++ windeeco/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { Text } from 'Home';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Testando</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render(){
+    return(
+      <View>
+        <Text> Anne </Text>
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
